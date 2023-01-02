b40d181162603a87fc90f733bc93ece54a86023b
nickecsimsw <nick.kim@3i.ai>
2023-01-02 15:55:23 +0900

    Initial Commit

---
+++ test1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by jinhwan on 2023/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by jinhwan on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
